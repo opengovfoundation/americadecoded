9cba085c17613a12d2ebdb645bbbbb3612d8c684
Chris Birk <cmbirk@gmail.com>
2013-11-05 14:29:14 -0500

    add map and instances, some basic content

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 var map, paper, pageWidth = 800;
+var instances = [];
 
 var drawIt = function(){
 	paper = new ScaleRaphael('plot-map-container', 950, 650);
@@ -6,4 +7,16 @@ var drawIt = function(){
 	paper.scaleAll(pageWidth/map.width);
 }
 
-document.onload = drawIt();
+$(document).ready(function(){
+	drawIt();
+
+	$.getJSON('js/instances.json', function(data){
+		instances = data.features;
+
+		$.each(instances, function(){
+			map.plot(this.geometry.coordinates[0], this.geometry.coordinates[1], this.geometry.properties["site-name"]);
+			map.darkenState(this.geometry.properties["place-state"], .1);
+		});
+	});
+
+});
