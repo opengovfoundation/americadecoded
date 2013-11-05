a7e84145aa0e29cc106201b2c43cdd142d54eb02
Chris Birk <cmbirk@gmail.com>
2013-11-05 16:38:01 -0500

    add sticky header and localscroll

--- js/main.js
+++ js/main.js
@@ -19,4 +19,13 @@ $(document).ready(function(){
 		});
 	});
 
+	$(".navbar").sticky({topSpacing:0});
+    
+    $(".navbar").localScroll({
+        axis : "y",
+        duration : 1000,
+        easing : "easeInOutExpo",
+        hash : true
+    });
+
 });
